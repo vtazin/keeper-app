49234f54426d9db72309237bb1ab15e6494492d1
Victor <vtazin@gmail.com>
2021-09-18 12:34:04 +0300

    Add PWA features

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
 
+import * as serviceWorkerRegistration from "./service-worker-registration";
+
+
 ReactDOM.render(
     <App/>,
     document.getElementById("root"));
 //Challenge. Render all the notes inside notes.ts as a seperate Note
 //component.
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
